0f05ebf34fad5cd07d54b6ef0af042672c6c8a2c
dusehrla <doyeon2010@naver.com>
2023-01-17 21:21:21 +0900

    +'의' 카테고리 화면 구성

--- app/src/main/java/com/example/guru16application/ClothingFragment.kt
+++ app/src/main/java/com/example/guru16application/ClothingFragment.kt
@@ -1,10 +1,17 @@
 package com.example.guru16application
 
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.widget.SearchView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,15 +24,44 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class ClothingFragment : Fragment() {
+    lateinit var mainActivity: MainActivity
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        mainActivity = context as MainActivity
+    }
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
+    lateinit var searchEdt: EditText
+    lateinit var searchBtn: Button
+    lateinit var myHelper: myDBHelper
+    lateinit var sqlDB: SQLiteDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
+        }
+
+        searchEdt = requireView().findViewById(R.id.searchEdt)
+        searchBtn = requireView().findViewById(R.id.searchBtn)
+
+        myHelper = myDBHelper(mainActivity.applicationContext)
+    }
+
+    inner class myDBHelper(context: Context) : SQLiteOpenHelper(context, "clothesDB", null, 1) {
+        override fun onCreate(p0: SQLiteDatabase?) {
+            p0!!.execSQL("CREATE TABLE clothesTBL (cName CHAR(20) PRIMARY KEY);")
+        }
+
+        override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
+            p0!!.execSQL("DROP TABLE IF EXISTS clothesTBL")
+            onCreate(p0)
         }
     }
 
