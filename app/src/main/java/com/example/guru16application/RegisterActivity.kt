8e015259c19eff66d9344a22686cbf9ad410990f
HanHyeBeen <vvv5974033@gmil.com>
2023-01-18 16:40:39 +0900

    login, register, db

--- app/src/main/java/com/example/guru16application/RegisterActivity.kt
+++ app/src/main/java/com/example/guru16application/RegisterActivity.kt
@@ -1,11 +1,43 @@
 package com.example.guru16application
 
+import android.database.sqlite.SQLiteDatabase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import com.example.studentmanagement.DBManager
 
 class RegisterActivity : AppCompatActivity() {
+    lateinit var dbManager: DBManager
+    lateinit var sqlitedb: SQLiteDatabase
+
+    lateinit var addName: EditText
+    lateinit var addTel: EditText
+    lateinit var addMail: EditText
+    lateinit var addPw: EditText
+    lateinit var btnRegister: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        addName = findViewById(R.id.addName)
+        addTel = findViewById(R.id.addTel)
+        addMail = findViewById(R.id.addMail)
+        addPw = findViewById(R.id.addPw)
+        btnRegister = findViewById(R.id.btnRegister)
+
+        dbManager = DBManager(this, "personDB", null, 1)
+
+        btnRegister.setOnClickListener {
+            var str_name: String = addName.text.toString()
+            var str_tel: String = addTel.text.toString()
+            var str_mail: String = addMail.text.toString()
+            var str_pw: String = addPw.text.toString()
+
+            sqlitedb = dbManager.writableDatabase
+            sqlitedb.execSQL("INSERT INTO person VALUES ('" + str_name + "'," + str_tel + "'," + str_mail + "'," + str_pw + "')")
+            sqlitedb.close()
+        }
     }
 }
