f49d3e4a64df630c4ea422e99c9ae45bbece7bb2
HanHyeBeen <vvv5974033@gmil.com>
2023-01-19 01:15:31 +0900

    login

--- app/src/main/java/com/example/guru16application/member/LoginActivity.kt
+++ app/src/main/java/com/example/guru16application/member/LoginActivity.kt
@@ -2,12 +2,22 @@ package com.example.guru16application.member
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import com.example.guru16application.R
 
 class LoginActivity : AppCompatActivity() {
-    // 에디트 텍스트 2개 버튼 2개 이미지 1개
+    lateinit var edit_id: EditText
+    lateinit var edit_pw: EditText
+    lateinit var btn_login: Button
+    lateinit var btn_register: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        btn_register.setOnClickListener {
+            //회원가입 페이지로 이동
+        }
     }
 }
