2455c82a460c378b95370241c1664076592a689b
HanHyeBeen <vvv5974033@gmil.com>
2023-01-25 16:32:04 +0900

    font, mypage

--- app/src/main/java/com/example/guru16application/member/Mypage.kt
+++ app/src/main/java/com/example/guru16application/member/Mypage.kt
@@ -2,11 +2,19 @@ package com.example.guru16application.member
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.guru16application.R
 
 class Mypage : AppCompatActivity() {
+    lateinit var btn_logout: Button
+    lateinit var btn_delete: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mypage)
+
+        btn_logout = findViewById(R.id.btn_logout)
+        btn_delete = findViewById(R.id.btn_delete)
     }
+
 }
